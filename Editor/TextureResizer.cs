247b2b03dca3f5707d3df22e1051c6965edb5c86
EvanMcCormick37 <e.kidmccorm@gmail.com>
2026-02-27 13:34:51 -0700

    sd

--- Editor/TextureResizer.cs
+++ Editor/TextureResizer.cs
@@ -6,6 +6,6 @@ public class TextureResozer : AssetPostprocessor
     void OnPreprocessTexture()
     {
         TextureImporter tex_importer = assetImporter as TextureImporter;
-        tex_importer.maxTextureSize = 256; // Set your desired default max size here
+        tex_importer.maxTextureSize = 256;
     }
 }
